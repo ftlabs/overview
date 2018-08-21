f8267c05cbb333654dadfc3c5a678747f6ade0a3
Sqyphen <tobias.fieldhouse@gmail.com>
2018-08-21 16:13:28 +0100

    updates templated var to let

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ hbs.registerHelper('imgPath', function(path) {
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
-var requestLogger = function(req, res, next) {
+let requestLogger = function(req, res, next) {
   debug("RECEIVED REQUEST:", req.method, req.url);
   next(); // Passing the request to the next handler in the stack.
 };
