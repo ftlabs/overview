ac0f838cba881ec0097bf8a59b97779214f35005
Sqyphen <tobias.fieldhouse@gmail.com>
2018-08-21 14:26:11 +0100

    allows daysOfArticles to accept aspect & facet params

--- modules/article.js
+++ modules/article.js
@@ -5,6 +5,28 @@ const CAPI_CONCURRENCE = process.env.hasOwnProperty('CAPI_CONCURRENCE')
 	? process.env.CAPI_CONCURRENCE
 	: 4;
 
+const defaultAspects = [ "audioVisual",
+							"editorial",
+							"images",
+							"lifecycle",
+							"location",
+							"master",
+							"metadata",
+							"nature",
+							"provenance",
+							"summary",
+							"title"
+						];
+const defaultFacets = [ "organisations",
+						"organisationsId",
+						"people", 
+						"peopleId",
+						"topics",
+						"topicsId"
+					];
+
+
+
 function searchByParams(params){
 	return fetchContent.search(params);
 }
@@ -25,7 +47,7 @@ function getByUuid(uuid){
 	return fetchContent.getArticle(uuid);
 }
 
-function getDaysOfRecentArticles(days = 1, aspects = [], facets = []){
+function getDaysOfRecentArticles(days = 1, aspects = defaultAspects, facets = defaultFacets){
 	return fetchContent.getRecentArticles(days, aspects, facets);
 }
 
--- routes/twentyfourhrs.js
+++ routes/twentyfourhrs.js
@@ -2,29 +2,6 @@ const express = require('express');
 const router = express.Router();
 const article = require('../modules/article');
 const parameters = require('../helpers/parameters');
- 
-
-const defaultAspects = ["audioVisual",
-						"editorial",
-						"images",
-						"lifecycle",
-						"location",
-						"master",
-						"metadata",
-						"nature",
-						"provenance",
-						"summary",
-						"title"
-					];
-const defaultFacets = ["organisations",
-						"organisationsId",
-						"people", 
-						"peopleId",
-						"topics",
-						"topicsId"
-					];
-
-
 
 
 // paths
@@ -33,30 +10,32 @@ router.get('/', async (req, res, next) => {
 });
 
 router.get('/1', async (req, res, next) => {
-	let results = await article.getDaysOfRecentArticles(1, defaultAspects, defaultFacets);
+	let results = await article.getDaysOfRecentArticles(1);
 	res.render("twentyfourhrs/one", { results: results });
 });
 
 router.get('/2', async (req, res, next) => {
-	let results = await article.getDaysOfRecentArticles(1, defaultAspects, defaultFacets);
+	let results = await article.getDaysOfRecentArticles(1);
 	res.render("twentyfourhrs/two", { results: results });
 });
 
 router.get('/2a', async (req, res, next) => {
-	let results = await article.getDaysOfRecentArticles(2, defaultAspects, defaultFacets);
+	let results = await article.getDaysOfRecentArticles(2);
 	results = parameters.limitReturn(results, 100);
 	res.render("twentyfourhrs/twoA", { results: results });
 });
 
 router.get('/3', async (req, res, next) => {
-	let results = await article.getDaysOfRecentArticles(1, defaultAspects, defaultFacets);
+	let results = await article.getDaysOfRecentArticles(1);
 	res.render("twentyfourhrs/three", { results: results });
 });
 
 
 // endpoints
 router.get('/daysOfArticles', async (req, res, next) => {
-	let results = await article.getDaysOfRecentArticles(req.query.days, defaultAspects, defaultFacets);
+	let aspects = (req.query.aspects) ? req.query.aspects : undefined;
+	let facets = (req.query.facets) ? req.query.facets : undefined;
+	let results = await article.getDaysOfRecentArticles(req.query.days, aspects, facets);
 	res.json( results );
 });
 
