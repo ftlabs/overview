681cacfbdf32d5b65f4dba0dbbde0fc08eb466cb
Sqyphen <tobias.fieldhouse@gmail.com>
2018-08-23 11:36:39 +0100

    adds unsorted count list of relatedTopics, removes inital loop over articles (only 1 required)

--- helpers/array.js
+++ helpers/array.js
@@ -4,11 +4,20 @@ function unique(arrays){
 	return ( [ ...new Set( [].concat(...arrays) ) ] );
 }
 
+function uniqueCount(array){
+	let count = {};
+	for(let i = 0; i < array.length; i++){
+		count[array[i]] = 1 + (count[array[i]] || 0);
+	}
+	return count;
+}
+
 function matching(arrays){
 	return [];
 }
 
 
 module.exports = {
-	unique
+	unique,
+	uniqueCount
 };
