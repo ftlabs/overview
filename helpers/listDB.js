88f965642d20e5882aa81af78a57737dd18deaac
Jamie.Brown <jamiebrown201@hotmail.com>
2018-08-29 13:52:12 +0100

    add brackets

--- helpers/listDB.js
+++ helpers/listDB.js
@@ -2,7 +2,9 @@ const lists = JSON.parse(process.env.LIST_IDS);
 
 function getId(sectionName) {
   const list = lists[sectionName];
-  if (list) return list;
+  if (list) {
+    return list;
+  }
   throw new Error("list ID does not exist in env variable");
 }
 
