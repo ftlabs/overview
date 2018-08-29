77c2d16d298bf5e504865990cd89d69544285d1d
Jamie.Brown <jamiebrown201@hotmail.com>
2018-08-29 13:38:25 +0100

    get rid of json

--- routes/timeline.js
+++ routes/timeline.js
@@ -17,33 +17,4 @@ router.get("/latestData", async (req, res, next) => {
   }
 });
 
-const json = {
-  title: {
-    text: {
-      headline: "The week that was"
-    }
-  },
-  events: [
-    {
-      media: {
-        url: "{{ static_url }}/img/examples/houston/family.jpg",
-        caption:
-          "Houston's mother and Gospel singer, Cissy Houston (left) and cousin Dionne Warwick.",
-        credit:
-          "Cissy Houston photo:<a href='http://www.flickr.com/photos/11447043@N00/418180903/'>Tom Marcello</a><br/><a href='http://commons.wikimedia.org/wiki/File%3ADionne_Warwick_television_special_1969.JPG'>Dionne Warwick: CBS Television via Wikimedia Commons</a>"
-      },
-      start_date: {
-        month: "8",
-        day: "9",
-        year: "1963"
-      },
-      text: {
-        headline: "A Musical Heritage",
-        text:
-          "<p>Born in New Jersey on August 9th, 1963, Houston grew up surrounded by the music business. Her mother is gospel singer Cissy Houston and her cousins are Dee Dee and Dionne Warwick.</p>"
-      }
-    }
-  ]
-};
-
 module.exports = router;
