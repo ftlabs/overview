2e33b9e830b450340caeb5c7cc7ff0b795a54449
Sqyphen <tobias.fieldhouse@gmail.com>
2018-09-04 10:23:31 +0100

    adds inital script lib

--- static/js/heartbeat.js
+++ static/js/heartbeat.js
@@ -1,11 +1,27 @@
+class Heartbeat {
 
-const main = document.getElementsByTagName('main')[0];
+	constructor(type){
+		this.type = type;
+	}
 
-function init(){
-	
+	init(data, target){
+		this.datum = data;
+		this.datumTarget = target;
+
+		var fn = this[this.type];
+		if(typeof fn === "function") this[this.type]();
+	}
+
+	prepareData(){
+		return JSON.parse(data
+			.replace(/&quot;&gt;/g, '>', )
+			.replace(/&lt;/g, '<', )
+			.replace(/&gt;/g, '>', )
+			.replace(/&quot;/g, '"', )
+			.replace(/&amp;/g, '&', ));
+	}
+
+	one(){
+		console.log('one');
+	}
 }
-
-function addListeners(){
-}
-
-init();
