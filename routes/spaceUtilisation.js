6ca6d6b8d64622c3774cb8c6e84f2c575712d0ad
Jamie.Brown <jamiebrown201@hotmail.com>
2018-09-26 10:44:40 +0100

    hexagon

--- routes/spaceUtilisation.js
+++ routes/spaceUtilisation.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/hexagon", (req, res, next) => {
-  res.render("spaceUtilisation/hexagon");
+const spaceUtilisationService = require("../lib/spaceUtilisation");
+
+router.get("/hexagon", async (req, res, next) => {
+  let articles = await spaceUtilisationService.getData();
+  // articles = chunkArray(articles, 4);
+  res.render("spaceUtilisation/hexagon", { articles });
 });
+
+function chunkArray(myArray, chunk_size) {
+  var index = 0;
+  var arrayLength = myArray.length;
+  var tempArray = [];
+
+  for (index = 0; index < arrayLength; index += chunk_size) {
+    myChunk = myArray.slice(index, index + chunk_size);
+    // Do something if you want with the group
+    tempArray.push(myChunk);
+  }
+
+  return tempArray;
+}
 
 module.exports = router;
