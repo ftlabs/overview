1865acb4e1356a71d9470f74b5777d790c2537f9
Sqyphen <tobias.fieldhouse@gmail.com>
2018-10-12 15:55:30 +0100

    inital basic layout of top topic articles a-la ft.com

--- routes/facetsWithArticles.js
+++ routes/facetsWithArticles.js
@@ -43,6 +43,32 @@ router.get('/charts/:template/:facet/:days', async (req, res, next) => {
 	});
 });
 
+router.get('/articlesAggregation/visual_1', async (req, res, next) => {
+	const days = ( req.query.days ) ? req.query.days : 1;
+	const results = await article.getArticlesAggregation( days );
+
+	const genreNews = results.aggregationsByGenre['genre:genre:News'];
+	const topics = genreNews.correlationAnalysis.primaryTheme.topics;
+	let data = topics.map(topic => { return { name: topic[0] }; } );
+
+	data.forEach(topic => {
+		const articlesIDs = genreNews.articlesByMetadataCsv[`primaryTheme:topics:${topic.name}`];
+
+		topic['articles'] = articlesIDs.map(article => {
+			return genreNews.articlesByUuid[article];
+		});
+
+		topic['articles'] = topic['articles'].splice(0,2);
+	});
+
+	data = data.splice(0,3);
+
+	res.render("facetsWithArticles/articlesAggregation/visual_1", {
+		data: data,
+		days: days
+	} );
+});
+
 
 // endpoints
 router.get('/relatedContent', async (req, res, next) => {
@@ -68,6 +94,8 @@ router.get('/articlesAggregation', async (req, res, next) => {
 	const results = await article.getArticlesAggregation( days );
 	res.json( results );
 });
+
+
 
 
 
