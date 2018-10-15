d3acb7c6a70a4f2519fd8f2c4c0c9ab1c93e21f8
Jamie.Brown <jamiebrown201@hotmail.com>
2018-10-15 15:45:39 +0100

    restructure api

--- routes/aggregatedMetadataExperiments.js
+++ routes/aggregatedMetadataExperiments.js
@@ -20,6 +20,13 @@ router.get("/particle", async (req, res, next) => {
   });
 });
 
+router.get("/hexagon", async (req, res, next) => {
+  const content = await structureData(req);
+  res.render("aggregatedMetadataExperiments/hexagon", {
+    content
+  });
+});
+
 async function structureData(req) {
   const days = req.query.days ? req.query.days : 1;
   const facet = req.query.facet ? req.query.facet : "topics";
@@ -30,19 +37,67 @@ async function structureData(req) {
   }
 
   let results = await article.getArticlesAggregation(days);
-  results =
-    results.aggregationsByGenre["genre:genre:News"].correlationAnalysis
-      .primaryTheme;
+
+  results = results.aggregationsByGenre["genre:genre:News"];
+
+  //   maybe add people
+  const correlationAnalysis = results.correlationAnalysis.primaryTheme;
   let content = [];
-  Object.keys(results).forEach(facet => {
-    results[facet].forEach(theme => {
+  Object.keys(correlationAnalysis).forEach(facet => {
+    correlationAnalysis[facet].forEach(theme => {
+      const articles = getArticleData(
+        results.articlesByMetadataCsv[`primaryTheme:${facet}:${theme[0]}`],
+        results
+      );
+
+      const metadata = metadataCount(articles);
+
       content = [
         ...content,
-        { name: theme[0], size: `${theme[1] * 100}px`, score: theme[1] }
+        {
+          name: theme[0],
+          size: `${theme[1] * 100}px`,
+          score: theme[1],
+          articles,
+          metadata
+        }
       ];
     });
   });
+
+  console.log(JSON.stringify(content));
   return content;
+}
+
+function getArticleData(articleUUIDs, results) {
+  return articleUUIDs.map(uuid => results.articlesByUuid[uuid]);
+}
+
+function metadataCount(articles) {
+  const validTagSections = ["people", "organisations"];
+  let count = [];
+  articles.forEach(article => {
+    validTagSections.forEach(tagSections => {
+      const section = article.metadata[tagSections];
+      if (section) {
+        section.forEach(tag => {
+          const newTag = tag.term.name;
+          if (count.find(existingTag => existingTag.name === newTag)) {
+            count = count.map(existingTag => {
+              if (existingTag.name === newTag) {
+                return { name: existingTag.name, count: existingTag.count + 1 };
+              } else {
+                return existingTag;
+              }
+            });
+          } else {
+            count = [...count, { name: newTag, count: 1 }];
+          }
+        });
+      }
+    });
+  });
+  return count;
 }
 
 module.exports = router;
