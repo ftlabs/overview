a6044c9ec7f201e71cfc0aa86d1c124a3c202601
Chris Gathercole <chris.gathercole@ft.com>
2018-12-06 15:15:42 +0000

    comparing and classifying two years of topics

--- routes/year.js
+++ routes/year.js
@@ -9,6 +9,219 @@ router.get('/', (req, res, next) => {
   res.render("year");
 });
 
+function constructQueryParamsForYearTopicCounts( year=2018 ){
+  const queryString = [
+    `lastPublishDateTime:>${year}-01-01T00:00:00Z`,
+    `lastPublishDateTime:<${year+1}-01-01T00:00:00Z`
+  ].join(' and ');
+
+  const queryParams = {
+    queryString,
+    maxResults : 1,
+    includeCapi: false,
+  };
+
+  return queryParams;
+}
+
+function fetchSapiTopicSummary( year ){
+  const queryParams = constructQueryParamsForYearTopicCounts( year );
+  sapiV1CapiV2.search
+  return sapiV1CapiV2.search( queryParams )
+  .then( sapiResult => {
+    const v1AnnosByTaxonomy = {};
+
+    if ( sapiResult
+      && sapiResult.sapiObj
+      && sapiResult.sapiObj.sapiObj
+      && sapiResult.sapiObj.sapiObj.results
+      && sapiResult.sapiObj.sapiObj.results.length > 0
+      && sapiResult.sapiObj.sapiObj.results[0]
+      && sapiResult.sapiObj.sapiObj.results[0].facets
+    ) {
+      sapiResult.sapiObj.sapiObj.results[0].facets.forEach( facet => {
+        const taxonomy = facet.name;
+        if (!v1AnnosByTaxonomy.hasOwnProperty(taxonomy)) {
+          v1AnnosByTaxonomy[taxonomy] = [];
+        }
+        facet.facetElements.forEach(element => {
+          element.nameCsv = `${taxonomy}:${element.name}`;
+          v1AnnosByTaxonomy[taxonomy].push(element);
+        });
+      })
+    }
+
+    return v1AnnosByTaxonomy;
+  })
+  .then( v1AnnosByTaxonomy => {
+    return {
+      year,
+      v1AnnosByTaxonomy
+    }
+  })
+  ;
+}
+
+router.get('/topics/:year', async (req, res, next) => {
+	 try {
+     const year = req.params.year;
+	   const searchResponse = await fetchSapiTopicSummary( Number(year) );
+	   res.json( searchResponse );
+   } catch( err ){
+     res.json( { error: err.message, });
+   }
+});
+
+router.get('/topics/:year1/:year2', (req, res, next) => {
+	 try {
+     const years = [ Number(req.params.year1), Number(req.params.year2) ].sort();
+     const searchPromises = years.map( year => { return fetchSapiTopicSummary(year); });
+     Promise.all(searchPromises)
+     .then( data => {
+       res.json( data );
+     })
+     .catch( err => {
+       throw err;
+     })
+   } catch( err ){
+     res.json( { error: err.message, });
+   }
+});
+
+function compareYearsTopics( searchResponses ){
+  const combinedByTaxonomy = {};
+  const years = searchResponses.map( r => { return r.year; } );
+  searchResponses.forEach( (searchResponse, responseIndex) => {
+    const year = searchResponse.year;
+    const taxonomies = Object.keys( searchResponse.v1AnnosByTaxonomy );
+    taxonomies.forEach( taxonomy => {
+      if (!combinedByTaxonomy.hasOwnProperty(taxonomy)) {
+        combinedByTaxonomy[taxonomy] = {};
+      }
+      const taxonomyAnnos = combinedByTaxonomy[taxonomy];
+      searchResponse.v1AnnosByTaxonomy[taxonomy].forEach( anno => {
+        const name = anno.name;
+        if (! taxonomyAnnos.hasOwnProperty(name)) {
+          taxonomyAnnos[name] = {
+            name,
+            nameCsv: anno.nameCsv,
+            counts : [0,0]
+          };
+        }
+        taxonomyAnnos[name].counts[responseIndex] = anno.count;
+      });
+
+      
+    });
+  });
+
+  return {
+    years,
+    combinedByTaxonomy
+  };
+}
+
+router.get('/topics/compare/:year1/:year2', (req, res, next) => {
+	 try {
+     const years = [ Number(req.params.year1), Number(req.params.year2) ].sort();
+     const searchPromises = years.map( year => { return fetchSapiTopicSummary(year); });
+     Promise.all(searchPromises)
+     .then( searchResponses => {
+       return compareYearsTopics(searchResponses);
+     })
+     .then( data => {
+       res.json( data );
+     })
+     .catch( err => {
+       throw err;
+     })
+   } catch( err ){
+     res.json( { error: err.message, });
+   }
+});
+
+const defaultComparisonParams = {
+  minCount : 10,
+  minFractionDelta : 0.2
+};
+
+function classifyComparedTopics( comparedTopics ){
+  const classificationsByTaxonomy = {};
+  const taxonomies = Object.keys( comparedTopics.combinedByTaxonomy );
+  taxonomies.forEach( taxonomy => {
+    classificationsByTaxonomy[taxonomy] = {
+      increasing : [],
+      decreasing : [],
+      littleChange: []
+    };
+    const annoNames = Object.keys( comparedTopics.combinedByTaxonomy[taxonomy] );
+    annoNames.forEach( name => {
+      const anno = comparedTopics.combinedByTaxonomy[taxonomy][name];
+      const maxCount = Math.max( ...anno.counts );
+      const delta = anno.counts[1] - anno.counts[0];
+      const fractionDelta = delta / maxCount;
+
+      anno.maxCount = maxCount;
+      anno.delta = delta;
+      anno.fractionDelta = fractionDelta;
+      anno.absFractionDelta = Math.abs( fractionDelta );
+
+      if (maxCount >= defaultComparisonParams.minCount){
+        if (fractionDelta <= - defaultComparisonParams.minFractionDelta) {
+          classificationsByTaxonomy[taxonomy].decreasing.push(anno);
+        } else if (fractionDelta >= defaultComparisonParams.minFractionDelta) {
+          classificationsByTaxonomy[taxonomy].increasing.push(anno);
+        } else {
+          classificationsByTaxonomy[taxonomy].littleChange.push(anno);
+        }
+      }
+    });
+
+    ['increasing', 'decreasing', 'littleChange'].forEach( category => {
+      classificationsByTaxonomy[taxonomy][category].sort( (a,b) => {
+        if( a.absFractionDelta > b.absFractionDelta ){ return -1; }
+        else if( a.absFractionDelta < b.absFractionDelta ){ return +1; }
+        else { return 0; };
+      });
+    });
+
+  });
+
+  return {
+    description: [
+      'comparing topic counts across two years, grouped by taxonomy',
+      'classifying into increasing, decreasing, and little change',
+      'ignoring small topics',
+      'sorted by biggest proportional change'
+    ],
+    years : comparedTopics.years,
+    classificationsByTaxonomy,
+    comparisonParams: defaultComparisonParams
+  };
+}
+
+router.get('/topics/classify/:year1/:year2', (req, res, next) => {
+	 try {
+     const years = [ Number(req.params.year1), Number(req.params.year2) ].sort();
+     const searchPromises = years.map( year => { return fetchSapiTopicSummary(year); });
+     Promise.all(searchPromises)
+     .then( searchResponses => {
+       return compareYearsTopics(searchResponses);
+     })
+     .then( comparisons => {
+       return classifyComparedTopics( comparisons );
+     })
+     .then( data => {
+       res.json( data );
+     })
+     .catch( err => {
+       throw err;
+     })
+   } catch( err ){
+     res.json( { error: err.message, });
+   }
+});
+
 // function constructSearchParamsFromRequest( urlParams={}, bodyParams={} ){
 // 	const params = {};
 // 	// string params
